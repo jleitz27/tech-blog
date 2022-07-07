7abdcdc8a79c0cba5ba59532baa856ebe607c420
Jason L <jkleitz27@gmail.com>
2022-07-07 12:08:19 -0400

    updated routes but need to test to confirm they work

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes');
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes');
 
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
